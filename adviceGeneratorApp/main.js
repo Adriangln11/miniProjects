ee25337533588c25697be96e125dcf980aaceed2
AdrianDiaz <cesarqqu@gmail.com>
2022-12-24 14:42:39 -0600

    adviceGenerator

---
+++ adviceGeneratorApp/main.js
@@ -0,0 +1,21 @@
+const adviceIdElemet = document.getElementById("advice-id")
+const adviceElement = document.getElementById("advice")
+const dice = document.getElementById("dice-btn")
+let url = "https://api.adviceslip.com/advice"
+let id
+let advice
+const fetching = (url) => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        id = data.slip.id
+        advice = data.slip.advice
+        adviceIdElemet.innerText = "Advice  #" + id
+        adviceElement.innerText = `\"${advice}\" `
+        console.log(id, advice)
+    })}
+
+dice.addEventListener("click", function(e) {
+    fetching(url)
+   
+})
