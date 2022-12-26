c29e3e2f8b5d4f0573b4696375dec8c1c4304228
AdrianDiaz <cesarqqu@gmail.com>
2022-12-26 15:57:10 -0600

    quizzApp improves

--- quizzApp/main.js
+++ quizzApp/main.js
@@ -1,38 +1,119 @@
-const startBtn = document.querySelector("#btn-start")
-const questionContainer = document.querySelector(".question-container")
-const question = document.querySelector(".question")
-const optionContainer = document.querySelector(".options-container")
-const option1 = document.querySelector("#option1")
-const option2 = document.querySelector("#option2")
-const option3 = document.querySelector("#option3")
+const questionElement = document.querySelector("#question");
+const listOptionsElement = document.querySelector("#list-options")
+const hitsElement = document.querySelector("#hits")
+const checkBtn = document.querySelector("#check-btn")
+const resetBtn = document.querySelector("#reset-btn")
+const correctAnswerElement = document.querySelector("#correct-answer")
+const answerDiv = document.querySelector("#answer-div")
+let correctAnswer
+let questionCounter = 0
+let score = 0
+let totalQuestions = 2
 
-let questionsArr = [
-    {
-        question:"this is the question",
-        options: ["option1", "option2", "option3"],
-        answer: 1
-    },
-    {
-        question:"this is another question",
-        options: ["option4", "option5", "option6"],
-        answer: 1
-    },
-]
-let random = Math.floor(Math.random() * 2)
-console.log(random)
+function eventListeners(){
+    checkBtn.addEventListener("click",checkingAnswer)
+    resetBtn.addEventListener("click", resetGame)
+}
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    hitsElement.innerText = score + "/" + totalQuestions 
+    fetching()
+    eventListeners()
+   
+})
 
 
 
-const renderQuestion = () => {
-    questionContainer.style = "transform: translate(50%, -50%);"
-    // question.innerText = questionsArr[random].question
-    // option1.textContent = questionsArr[random].options[0]
-    // option2.innerText = questionsArr[random].options[1]
-    // option3.innerText = questionsArr[random].options[2]
-        
-    console.log(option1)
+async function fetching() {
+    const ApiUrl = "https://opentdb.com/api.php?amount=10"
+    const result = await fetch(ApiUrl)
+    const data = await result.json()
+    
+    renderQuestion(data.results[0])
 }
 
 
-startBtn.addEventListener("click", renderQuestion)
+function renderQuestion(data) {
+    correctAnswer = data.correct_answer
+    let incorrectAnswers = data.incorrect_answers
+    let question = data.question
+    let totalOptions = incorrectAnswers
+    totalOptions.splice(Math.floor(Math.random() * (incorrectAnswers.length + 1)), 0, correctAnswer)
+
+    questionElement.innerText =  question;
+    correctAnswerElement.textContent = ""
+    questionElement.style.display = "block"
+    listOptionsElement.innerHTML =  `
+        ${totalOptions.map((option,index) =>`
+            <li> <span> ${index + 1}.</span> <small>${option}</small> </li>
+            `).join("")
+        }`
+        selecting()
+}
+
+function selecting(){
+    listOptionsElement.querySelectorAll("li").forEach(option => {
+        option.addEventListener("click", () => {
+            if (listOptionsElement.querySelector(".selected")) {
+                const selected = listOptionsElement.querySelector(".selected")
+                selected.classList.remove("selected")
+            }else{
+                option.classList.add("selected")
+            }
+        })
+    })
+}
+
+function checkingAnswer () {
+
+    if (listOptionsElement.querySelector(".selected")) {
+        let selected = listOptionsElement.querySelector(".selected small").textContent
+        if (selected == correctAnswer) {
+            score++
+            hitsElement.innerText = score + "/" + totalQuestions 
+            correctAnswerElement.innerText = "Correct!!"
+            answerDiv.style.display = "block"
+        }else{
+            correctAnswerElement.innerText = "Try Again :("
+            answerDiv.style.display = "block"
+        }
+        
+        checkCount()
+    }else{
+        correctAnswerElement.innerText = "Please Select An Option :( "
+        answerDiv.style.display = "block"
+    }
+}
+
+function checkCount() {
+    questionCounter++
+    if(questionCounter == totalQuestions){
+        console.log("game has over")
+        correctAnswerElement.innerText = "Game Over. Thanks for Play."
+        correctAnswerElement.style.display = "block"
+        listOptionsElement.style.display = "none"
+        questionElement.style.display = "none"
+        resetBtn.style.display = "inline-block"
+        checkBtn.style.display = "none"
+    }else{
+        
+        setTimeout(fetching, 500)
+    }
+}
+
+function resetGame(){
+    questionCounter = 0
+    score = 0
+    hitsElement.innerText = score + "/" + totalQuestions 
+    
+    correctAnswerElement.textContent = ""
+    resetBtn.style.display = "none"
+    
+    checkBtn.style.display = "inline-block"
+    listOptionsElement.style.display = "block"
+    renderQuestion()
+}
+        
+
+
 
